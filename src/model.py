45acb713c69e498483773a20b6823d64bf3c1bf6
Jeongsoo Ha <jeongsoo.Ha1210@gmail.com>
2024-12-22 00:23:16 +0900

    Update model.py

--- src/model.py
+++ src/model.py
@@ -9,7 +9,8 @@ from dataloader_iam import Batch
 
 # Disable eager mode
 tf.compat.v1.disable_eager_execution()
-tf.reset_default_graph()
+tf.compat.v1.reset_default_graph()
+
 
 class DecoderType:
     """CTC decoder types."""
